f1ddcbfb7df0f58aec9d2e95e28563d8b2541e23
Caleb King <caleb.king536@gmail.com>
2019-11-04 10:20:27 -0700

    used api.createItems in shopping-list

--- src/index.js
+++ src/index.js
@@ -2,19 +2,17 @@ import $ from 'jquery';
 import api from './api';
 import 'normalize.css';
 import './index.css';
+import store from './store';
 
 import shoppingList from './shopping-list';
 
 const main = function () {
 
-  api.createItem('pears')
-  .then(res => res.json())
-  .then((newItem) => {
-    return api.getItems();
-  })
+  api.getItems()
   .then(res => res.json())
   .then((items) => {
-    console.log(items);
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
   });
 
   shoppingList.bindEventListeners();
--- src/shopping-list.js
+++ src/shopping-list.js
@@ -1,5 +1,5 @@
 import $ from 'jquery';
-
+import api from './api';
 import store from './store';
 import item from './item';
 
@@ -51,8 +51,12 @@ const handleNewItemSubmit = function () {
     event.preventDefault();
     const newItemName = $('.js-shopping-list-entry').val();
     $('.js-shopping-list-entry').val('');
-    store.addItem(newItemName);
-    render();
+    api.createItem(newItemName)
+      .then(res => res.json())
+      .then(newitem => {
+        store.addItem(newitem);
+        render();
+      });
   });
 };
 
