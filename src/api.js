9ab6ffaf07e5b0779357e2b58822da631e9e8cc9
Caleb King <caleb.king536@gmail.com>
2019-11-04 09:09:12 -0700

    updated base URL

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://thinkful-list-api.herokuapp.com/[yourname]';
+const BASE_URL = 'https://thinkful-list-api.herokuapp.com/caleb';
 
 const getItems = function () {
   return Promise.resolve('A successful response!');
