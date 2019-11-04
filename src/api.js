76d2a6e0c324e61eea53513b49ee86d7066f5c04
Anugrah Lambogo <lambogoai@gmail.com>
2019-11-04 10:40:51 -0600

    finished number 4

--- src/api.js
+++ src/api.js
@@ -1,9 +1,26 @@
+/* eslint-disable indent */
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/caleb';
 
 const getItems = function () {
-  return Promise.resolve('A successful response!');
+  return fetch(`${BASE_URL}/items`)
 };
 
+const createItem = function (name) {
+  let newItem = {
+    name: name
+  };
+
+  let newItemJSON = JSON.stringify(newItem);
+  return fetch(`${BASE_URL}/items`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      },
+    body: newItemJSON
+  });
+}
+
 export default {
-  getItems
+  getItems,
+  createItem
 };
--- src/index.js
+++ src/index.js
@@ -6,6 +6,17 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
+
+  api.createItem('pears')
+  .then(res => res.json())
+  .then((newItem) => {
+    return api.getItems();
+  })
+  .then(res => res.json())
+  .then((items) => {
+    console.log(items);
+  });
+
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
